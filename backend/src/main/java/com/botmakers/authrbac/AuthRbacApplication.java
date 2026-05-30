90052a6a740e7e4ada76e0e3e3aa2f276686bf5d
Shristirajpoot <shristirajpoot369@gmail.com>
2026-05-30 22:15:00 +0530

    chore: initialize backend maven configurations and database entities

---
+++ backend/src/main/java/com/botmakers/authrbac/AuthRbacApplication.java
@@ -0,0 +1,12 @@
+package com.botmakers.authrbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthRbacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthRbacApplication.class, args);
+    }
+}
